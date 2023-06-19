53bccd3953082bbdb7cc39819c5db1f1339e66bb
Christian Boulanger <info@bibliograph.org>
2023-06-19 09:48:02 +0200

    Update figures and code

--- scripts/pyvis.py
+++ scripts/pyvis.py
@@ -1,3 +1,5 @@
+import os.path
+
 from py2neo import Graph, Path, Node, Relationship, walk
 from pyvis.network import Network
 from IPython.display import display, HTML, display_png
@@ -59,7 +61,9 @@ def create_or_update_network(graph: Graph,
 
 def draw_network(net: Network,
                  title: str = None,
+                 caption: str = None,
                  file: str = None,
+                 url: str = None,
                  prev_url: str = None,
                  next_url: str = None,
                  link_only: bool = False):
@@ -70,9 +74,14 @@ def draw_network(net: Network,
     if title is not None:
         html = html.replace("<head>", f'<head><title>{title}</title>')
         html = html.replace("<body>", f'<body><h1 style="text-align:center">{title}</h1>')
+    # optional: caption
+    if caption is not None:
+        html = html.replace("</body>", f'\n<div style="text-align:center">{caption}</div>\n</body>')
     # optional: back & forward links
     if prev_url or next_url:
         nav_bar = '<div style="text-align:center">'
+        if caption:
+            nav_bar = "<hr/>" + nav_bar
         if prev_url:
             nav_bar += f'[&nbsp;<a href="{prev_url}">Previous</a>&nbsp;]'
         if next_url:
@@ -87,8 +96,10 @@ def draw_network(net: Network,
         else:
             raise RuntimeError("Unsupported file extension")
     # optional: return a link only
-    if link_only and file:
-        display(HTML(f'<a href="{file}" target="_blank">Click here to open the graph.</a>'))
+    if link_only and url:
+        display(HTML(f'<a href="{url}" target="_blank">Click here to open {os.path.basename(file)}.</a>'))
+    elif link_only and file:
+        display(HTML(f'<a href="{file}" target="_blank">Click here to open {os.path.basename(file)}.</a>'))
     else:
         display(HTML(html))
 
@@ -109,7 +120,7 @@ def draw(graph: Graph,
     net = create_or_update_network(graph, query, height=height, seed=seed, auto_rel_label=auto_rel_label, **kwargs)
     return draw_network(net, file=file, link_only=link_only, title=title)
 
-def create_timeseries(graph: Graph, query: str, file_id:str, title: str = None, seed=5):
+def create_timeseries(graph: Graph, query: str, file_id:str, title: str = None, seed=5, file_prefix="docs/"):
     start_year = 1974
     end_year = 2023
     num_ranges = 5
@@ -120,9 +131,10 @@ def create_timeseries(graph: Graph, query: str, file_id:str, title: str = None, 
             decade_end = end_year
         net = create_or_update_network(graph, query, height="600", seed=seed,
                                        year_start=decade_start, year_end=decade_end)
-        url = f"figure/{file_id}-{decade_start}-{decade_end}.html"
+        file = f"{file_id}-{decade_start}-{decade_end}.html"
+        url = f"https://cboulanger.github.io/jls-bibliometry/{file}"
         prev_url = f"{file_id}-{decade_start-10}-{decade_start-1}.html" if i > 0 else None
         next_url = f"{file_id}-{decade_end+1}-{decade_end+10}.html" if i < (num_ranges - 1) else None
         draw_network(net, title=f"{title}, {decade_start} - {decade_end}",
                      prev_url=prev_url, next_url= next_url,
-                     file=url, link_only=True)
+                     file=f"{file_prefix}{file}", url=url, link_only=True)
